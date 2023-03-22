cb5983d64ea949a8677053ecd53683538cd5b481
bishengjie <255077145@qq.com>
2023-03-22 15:07:38 +0800

    feat: StartCoroutine协程

--- Assets/Scripts/DialogSystem.cs
+++ Assets/Scripts/DialogSystem.cs
@@ -7,15 +7,22 @@ using UnityEngine.UI;
 
 public class DialogSystem : MonoBehaviour
 {
-    [Header("UI组件")] 
+    [Header("UI组件")]
     public TMP_Text textLabel;
     public Image factImage;
 
-    [Header("文本文件")] 
-    public TextAsset textFile;
+    [Header("文本文件")] public TextAsset textFile;
 
     public int index;
+    public float textSpeed;
+
+    [Header("头像")] 
+    public Sprite face01, face02;
+    
+    private bool textFinished;
+
     private List<string> textList = new List<string>();
+
     void Awake()
     {
         GetTextFormFile(textFile);
@@ -23,8 +30,10 @@ public class DialogSystem : MonoBehaviour
 
     private void OnEnable()
     {
-        textLabel.text = textList[index];
-        index++;
+        // textLabel.text = textList[index];
+        // index++;
+        textFinished = true;
+        StartCoroutine(SetTextUI());
     }
 
     void Update()
@@ -35,10 +44,12 @@ public class DialogSystem : MonoBehaviour
             index = 0;
             return;
         }
-        if (Input.GetKeyDown(KeyCode.R))
+
+        if (Input.GetKeyDown(KeyCode.R) && textFinished)
         {
-            textLabel.text = textList[index];
-            index++;
+            // textLabel.text = textList[index];
+            // index++;
+            StartCoroutine(SetTextUI());
         }
     }
 
@@ -47,11 +58,38 @@ public class DialogSystem : MonoBehaviour
         textList.Clear();
         index = 0;
 
-       string[] lineDate= file.text.Split('\n');
-       foreach (var line in lineDate)
-       {
-           textList.Add(line);
-           print(line);
-       }
+        string[] lineDate = file.text.Split('\n');
+        foreach (var line in lineDate)
+        {
+            textList.Add(line);
+            print(line);
+        }
+    }
+
+    IEnumerator SetTextUI()
+    {
+        textFinished = false;
+        textLabel.text = "";
+
+        switch (textList[index])
+        {
+            case "A":
+                factImage.sprite = face01;
+                index++;
+                break;
+            case "B":
+                factImage.sprite = face02;
+                index++;
+                break;
+        }
+        
+        for (int i = 0; i < textList[index].Length; i++)
+        {
+            textLabel.text += textList[index][i];
+            yield return new WaitForSeconds(textSpeed);
+        }
+
+        textFinished = true;
+        index++;
     }
 }
